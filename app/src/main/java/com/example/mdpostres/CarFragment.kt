645a209d4732737a5bb8a874a95d7c2d532cc9cd
José Zambrano <zambranomoya74@gmail.com>
2022-09-06 10:06:17 +0200

    UX hide menu

--- app/src/main/java/com/example/mdpostres/CarFragment.kt
+++ app/src/main/java/com/example/mdpostres/CarFragment.kt
@@ -1,14 +1,20 @@
 package com.example.mdpostres
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.mdpostres.databinding.FragmentCarBinding
 
 
-class CarFragment : Fragment(R.layout.fragment_car) {
+class CarFragment : Fragment(R.layout.fragment_car), MenuProvider {
 
     private val args: CarFragmentArgs by navArgs()
 
@@ -33,7 +39,34 @@ class CarFragment : Fragment(R.layout.fragment_car) {
             findNavController().navigate(R.id.action_carFragment_to_confirmationFragment)
         }
 
+        //setHasOptionsMenu(true)
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
     }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menu.findItem(R.id.action_confirmation).isVisible = false
+        menu.findItem(R.id.confirmationFragment).isVisible = false
+    }
+
+    /**
+     * Called by the [MenuHost] when a [MenuItem] is selected from the menu.
+     *
+     * @param menuItem the menu item that was selected
+     * @return `true` if the given menu item is handled by this menu provider,
+     * `false` otherwise
+     */
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    /*  override fun onPrepareOptionsMenu(menu: Menu) {
+          menu.findItem(R.id.action_confirmation).isVisible = false
+          menu.findItem(R.id.confirmationFragment).isVisible = false
+          super.onPrepareOptionsMenu(menu)
+      }*/
 
     private fun sum(p: List<Product>): Float{
         var sum = 0f
--- app/src/main/java/com/example/mdpostres/ConfirmationFragment.kt
+++ app/src/main/java/com/example/mdpostres/ConfirmationFragment.kt
@@ -1,14 +1,15 @@
 package com.example.mdpostres
 
 import android.os.Bundle
+import android.view.*
+import androidx.core.view.MenuHost
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import com.example.mdpostres.databinding.FragmentConfirmationBinding
 
-class ConfirmationFragment : Fragment(R.layout.fragment_confirmation) {
+class ConfirmationFragment : Fragment(R.layout.fragment_confirmation),  MenuProvider{
 
     private lateinit var binding: FragmentConfirmationBinding
 
@@ -19,5 +20,31 @@ class ConfirmationFragment : Fragment(R.layout.fragment_confirmation) {
         binding.bntDone.setOnClickListener {
             findNavController().navigate(R.id.action_confirmationFragment_to_productsFragment)
         }
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    /**
+     * Called by the [MenuHost] to allow the [MenuProvider]
+     * to inflate [MenuItem]s into the menu.
+     *
+     * @param menu         the menu to inflate the new menu items into
+     * @param menuInflater the inflater to be used to inflate the updated menu
+     */
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menu.findItem(R.id.action_confirmation).isVisible = false
+        menu.findItem(R.id.confirmationFragment).isVisible = false
+    }
+
+    /**
+     * Called by the [MenuHost] when a [MenuItem] is selected from the menu.
+     *
+     * @param menuItem the menu item that was selected
+     * @return `true` if the given menu item is handled by this menu provider,
+     * `false` otherwise
+     */
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 }
